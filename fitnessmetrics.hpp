eece8d749dc6727dcc073928f96f399c6a402b56
al <alister.sanders@protonmail.com>
2021-12-06 08:50:07 +1300

    remove non-existent sigmoid function from header

--- fitnessmetrics.hpp
+++ fitnessmetrics.hpp
@@ -10,8 +10,6 @@
 namespace ft {
     using FitnessMetric = std::function<double(const cv::Mat&, const util::Box&)>;
 
-    double sigmoid(double x);
-
     FitnessMetric aspect_ratio(double aspect);
     FitnessMetric distance_to(util::Point<double> target);
     FitnessMetric relative_area(double ideal_area);
