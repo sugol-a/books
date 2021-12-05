6187aee1d7429bc2bd0de94336f95be112766f3b
al <alister.sanders@protonmail.com>
2021-12-06 08:45:04 +1300

    tidy up

--- books.cpp
+++ books.cpp
@@ -1,5 +1,4 @@
 #include <gtkmm.h>
-
 #include <mainwindow.hpp>
 #include <ui-resources.h>
 
@@ -8,26 +7,6 @@ static void on_startup(Glib::RefPtr<Gtk::Application> app) {
     app->add_window(*window);
     window->show();
 }
-
-// #include <iostream>
-// #include <image.hpp>
-// #include <imagestore.hpp>
-
-// int main(int argc, char *argv[]) {
-//     img::ImageStore store;
-//     store.populate(argv[1]);
-
-//     for (std::string filename : store.images()) {
-//         img::Image image(filename);
-//         std::cout << image.filename() << std::endl;
-//         std::cout << image.mat().size().width
-//                   << " "
-//                   << image.mat().size().height
-//                   << std::endl;
-//     }
-
-//     return 0;
-// }
 
 int main(int argc, char *argv[]) {
     auto resource_bundle = Glib::wrap(ui_res_get_resource());
