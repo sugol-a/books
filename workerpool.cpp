50c04d4d7951092c53b9730b12299efa99ea5fd6
al <alister.sanders@protonmail.com>
2021-12-08 04:41:19 +1300

    added automatic thread allocation

---
+++ workerpool.cpp
@@ -0,0 +1,49 @@
+#include <vector>
+#include <numeric>
+#include <workerpool.hpp>
+
+std::vector<size_t> thread_allocations(std::initializer_list<size_t> weights, size_t max_threads) {
+    size_t weight_sum = std::accumulate(weights.begin(), weights.end(), 0);
+
+    if (max_threads == 0) {
+        max_threads = std::thread::hardware_concurrency();
+    }
+
+    float scale = float(max_threads) / float(weight_sum);
+    std::vector<size_t> allocations;
+
+    // First pass: Assign thread allocation based on their weight
+    // and the available processor count
+    for (auto w : weights) {
+        allocations.push_back(w * scale);
+    }
+
+    // Second pass: If any allocations are zero, steal a thread from
+    // the max allocation
+    for (size_t& allocation : allocations) {
+        if (allocation == 0) {
+            auto max = std::max(allocations.begin(), allocations.end());
+
+            // Edge case: If this system doesn't have many hardware threads
+            // available, we may need to allocate a new thread here even
+            // though the total number of allocated threads will exceed the
+            // total number of hardware threads on the system
+            if (*max > 1) {
+                (*max)--;
+            }
+
+            allocation++;
+        }
+    }
+
+    // Third pass: Allocate any remaining threads
+    int remaining = max_threads - std::accumulate(allocations.begin(), allocations.end(), 0);
+    if (remaining > 0) {
+        for (size_t i = 0; i < size_t(remaining); i++) {
+            auto min = std::min(allocations.begin(), allocations.end());
+            (*min)++;
+        }
+    }
+
+    return allocations;
+}
--- workerpool.hpp
+++ workerpool.hpp
@@ -26,7 +26,22 @@ namespace worker {
             virtual std::shared_ptr<OutputQueue> output() = 0;
             virtual void set_input(std::shared_ptr<InputQueue> in) = 0;
             virtual void set_output(std::shared_ptr<OutputQueue> out) = 0;
-            virtual void run_workers() = 0;
-            virtual void join_all() = 0;
     };
+
+    /**
+     * Calculates weighted thread allocations. Generates a list of thread
+     * allocations, with each allocation approximately proportional to the
+     * corresponding weight in `weights`. `max_threads` sets the maximum number
+     * of threads to allocate - set it to 0 to automatically determine the
+     * number of processors on the system.
+     *
+     * The minimum thread allocation is 1; so on systems with few processors,
+     * this will allocate more threads than there are physical processors.
+     *
+     * Example:
+     * thread_allocations({2, 1}, 12)
+     *  -> { 8, 4 }
+     */
+    std::vector<size_t> thread_allocations(std::initializer_list<size_t> weights,
+                                           size_t max_threads = 0);
 }
