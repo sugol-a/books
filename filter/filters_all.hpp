609997f8782d773102f8cb1ef6f86bc251b19f83
al <alister.sanders@protonmail.com>
2021-12-17 00:16:14 +1300

    remove old filtering code

--- filter/filters_all.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <filter/bgr2grey.hpp>
-#include <filter/medianblur.hpp>
-#include <filter/normalise.hpp>
-#include <filter/dilate.hpp>
-#include <filter/threshold.hpp>
-#include <filter/canny.hpp>
