53978fe62c960442f11e7df254dac55f3a1e6506
al <alister.sanders@protonmail.com>
2021-12-17 07:19:41 +1300

    update buildsystem to allow for cross-compilation (win32)

--- scripts/dist-win32.sh
+++ scripts/dist-win32.sh
@@ -4,6 +4,8 @@ set -e
 
 APP_NAME="books"
 ARCH="x86_64-windows"
+LDD="x86_64-w64-mingw32-ldd"
+MINGW="/usr/x86_64-w64-mingw32"
 MESON_BUILD="$(dirname $0)/../meson.build"
 
 version=$(cat "$MESON_BUILD" | grep 'version' | head -n 1 | sed -e 's/\s*version\s*:\s*'"'"'\([[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+\).*/\1/')
@@ -24,10 +26,10 @@ echo " => Copying executable to $dist_dir/bin"
 cp -v "$executable" "$dist_dir/bin"
 
 echo " => Copying DLLs"
-ldd "$executable" | grep mingw | awk '{print $3}' | xargs -I {} cp -v {} "$dist_dir/bin"
+$LDD "$executable" | grep mingw | awk '{print $3}' | xargs -I {} cp -v {} "$dist_dir/bin"
 mkdir -pv "$dist_dir/lib/gdk-pixbuf-2.0/2.10.0/loaders"
-find /mingw64/lib/gdk-pixbuf-2.0/2.10.0/loaders -name '*.dll' | xargs -I {} cp -v {} "$dist_dir/lib/gdk-pixbuf-2.0/2.10.0/loaders"
-cp -v /mingw64/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache "$dist_dir/lib/gdk-pixbuf-2.0/2.10.0"
+find $MINGW/lib/gdk-pixbuf-2.0/2.10.0/loaders -name '*.dll' | xargs -I {} cp -v {} "$dist_dir/lib/gdk-pixbuf-2.0/2.10.0/loaders"
+#cp -v $MINGW/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache "$dist_dir/lib/gdk-pixbuf-2.0/2.10.0"
 
 echo " => Copying icons"
 [ -f icons.txt ] && rm icons.txt
@@ -38,15 +40,14 @@ echo "share/icons/Adwaita/scalable/ui/window-maximize-symbolic.svg" >> icons.txt
 echo "share/icons/Adwaita/scalable/ui/window-minimize-symbolic.svg" >> icons.txt
 
 # Other named icons
-find /mingw64/share/icons/Adwaita/scalable | grep -f <(grep 'icon-name' ../assets/ui/*.ui | sed -e 's/.*<property name="icon-name">\(\S*\)<.*/\1.svg/g') | sed -e 's,/mingw64/,,g' >> icons.txt
+find $MINGW/share/icons/Adwaita/scalable | grep -f <(grep 'icon-name' ../assets/ui/*.ui | sed -e 's/.*<property name="icon-name">\(\S*\)<.*/\1.svg/g') | sed -e 's,/mingw64/,,g' >> icons.txt
 
 mkdir "$dist_dir/share/icons/Adwaita"
 cat icons.txt | xargs -I {} dirname {} | sort | uniq | xargs -I {} mkdir -pv "$dist_dir/{}"
-cat icons.txt | xargs -I {} cp "/mingw64/{}" "$dist_dir/{}"
-#cp -r /mingw64/share/icons/{Adwaita,hicolor} "$dist_dir/share/icons"
+cat icons.txt | xargs -I {} cp "$MINGW/{}" "$dist_dir/{}"
 
 echo " => Copying schemas"
-cp /mingw64/share/glib-2.0/schemas/gschemas.compiled "$dist_dir/share/glib-2.0/schemas/gschemas.compiled"
+cp $MINGW/share/glib-2.0/schemas/gschemas.compiled "$dist_dir/share/glib-2.0/schemas/gschemas.compiled"
 
 echo " => Zipping"
 cd "$build_dir"
